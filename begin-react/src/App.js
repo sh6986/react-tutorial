07ceea39e755de3f6d4906ff653879c7d7cb5a8f
seunghyun_oh <okok960411@gmail.com>
2021-03-20 20:28:08 +0900

    작업내용 : 04. JSX 작업일시 : 2021.03.20 작업자 : 오승현

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import Hello from './Hello';
+import './App.css';
 
 function App() {
+  const name = 'react';
+  const style = {
+    backgroundColor: 'black',
+    color: 'aqua',
+    fontSize: 24,
+    padding: '1rem'
+  };
   return (
-    <div>
-      <Hello />
-      <Hello />
-      <Hello />
-    </div>
+    <>
+      {/*주석*/}
+      <Hello 
+        // 주석
+      />
+      <div
+        // 주석
+      style={style}>{name}</div>  
+      <div className="gray-box"></div>
+    </>
   );
 }
 
