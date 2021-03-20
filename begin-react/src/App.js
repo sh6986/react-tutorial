eefc49f4c277aabd0c5f137d983e9e998eaa76f4
seunghyun_oh <okok960411@gmail.com>
2021-03-20 23:41:24 +0900

    작업내용 : 13. 배열에 항목 추가하기 작업일시 : 2021.03.20 작업자 : 오승현

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import UserList from './UserList';
 
 
 function App() {
+  const users = [
+    {
+        id: 1,
+        username: 'velopert',
+        email: 'public.velopert@gmail.com'
+    },
+    {
+        id: 2,
+        username: 'tester',
+        email: 'tester@gmail.com'
+    },
+    {
+        id: 3,
+        username: 'liz',
+        email: 'liz@gmail.com'
+    }
+  ];
+
+  const nextId = useRef(4);
+
+  const onCreate = () => {
+    console.log(nextId.current);  // 4  
+    nextId.current += 1;
+  };
+
+
   return (
-    <UserList />
+    <UserList users={users}/>
   )
 }
 
