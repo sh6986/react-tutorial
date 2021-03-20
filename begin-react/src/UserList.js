34f6fe0620c1de1bdc0b3cf8841b96b8ba82a509
seunghyun_oh <okok960411@gmail.com>
2021-03-20 23:41:44 +0900

    Revert "작업내용 : 13. 배열에 항목 추가하기"
    This reverts commit eefc49f4c277aabd0c5f137d983e9e998eaa76f4.

--- begin-react/src/UserList.js
+++ begin-react/src/UserList.js
@@ -9,8 +9,24 @@ function User({user}) {
     
 }
 
-function UserList({users}) {
-    
+function UserList() {
+    const users = [
+        {
+            id: 1,
+            username: 'velopert',
+            email: 'public.velopert@gmail.com'
+        },
+        {
+            id: 2,
+            username: 'tester',
+            email: 'tester@gmail.com'
+        },
+        {
+            id: 3,
+            username: 'liz',
+            email: 'liz@gmail.com'
+        }
+    ];
 
     return (
         <div>
