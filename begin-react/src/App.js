4c4e9f4084a97563a8e0bc42eed82b37f5f28db3
seunghyun_oh <okok960411@gmail.com>
2021-03-20 21:24:16 +0900

    작업내용 : 07. useState를 통해 컴포넌트에서 바뀌는 값 관리하기 작업일시 : 2021.03.20 작업자 : 오승현

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -1,15 +1,11 @@
 import React from 'react';
-import Hello from './Hello';
-import Wrapper from './Wrapper';
+import Counter from './Counter';
 
 
 function App() {
   return (
-    <Wrapper>
-      <Hello name="react" color="red" isSpecial/>
-      <Hello color="pink"/>
-    </Wrapper>
-  );
+    <Counter/>
+  )
 }
 
 export default App;
