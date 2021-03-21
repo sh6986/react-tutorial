64cc8cf6f4613f3f77dd2589ff62e57b17759aaf
seunghyun_oh <okok960411@gmail.com>
2021-03-21 20:26:49 +0900

    작업내용 : 27. 클래스형 컴포넌트 소개 작업일시 : 2021.03.21 작업자 : 오승현

--- begin-react/src/Hello.js
+++ begin-react/src/Hello.js
@@ -1,17 +1,33 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-function Hello({color, name, isSpecial}) {
-    return (
-        <div style={{
-            color
-        }}>
-            {isSpecial && <b>*</b>}
-            안녕하세요 {name}
-        </div>
-    );
+class Hello extends Component {
+    static defaultProps = {
+        name: '이름없음',
+    };
+    render() {
+        const {color, isSpecial, name} = this.props;
+        return (
+            <div style={{color}}>
+                {isSpecial && <b></b>}
+                안녕하세요 {name}
+            </div>
+        );
+    }
 }
 
-Hello.defaultProps = {
-    name: '이름없음'
-}
+// function Hello({color, name, isSpecial}) {
+//     return (
+//         <div style={{
+//             color
+//         }}>
+//             {isSpecial && <b>*</b>}
+//             안녕하세요 {name}
+//         </div>
+//     );
+// }
+
+
+// Hello.defaultProps = {
+//     name: '이름없음'
+// }
 export default Hello;
--- begin-react/src/index.js
+++ begin-react/src/index.js
@@ -3,11 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Hello from './Hello';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Hello name="react" isSpecial/>,
   document.getElementById('root')
 );
 
