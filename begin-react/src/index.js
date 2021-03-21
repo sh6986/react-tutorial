efba37b86c0b11a4c8c0cb69665fb2b219f48412
seunghyun_oh <okok960411@gmail.com>
2021-03-21 19:11:15 +0900

    작업내용 : 23. Context API를 사용한 전역 값 관리 작업일시 : 2021.03.21 작업자 : 오승현

--- begin-react/src/index.js
+++ begin-react/src/index.js
@@ -3,11 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ContextSample from './ContextSample';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ContextSample />,
   document.getElementById('root')
 );
 
