49e96246d4f1ffc56d1532a2d16f0d4b7702d59d
seunghyun_oh <okok960411@gmail.com>
2021-03-21 18:27:45 +0900

    작업내용 : 21. useReducer - App에서 useReducer 사용하기 작업일시 : 2021.03.21 작업자 : 오승현

--- begin-react/src/index.js
+++ begin-react/src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Counter from './Counter';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <Counter />,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
