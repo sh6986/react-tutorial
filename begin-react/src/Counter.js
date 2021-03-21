ae7a43b8daa69fccd9cd23d0a9170d14c92ddbc2
seunghyun_oh <okok960411@gmail.com>
2021-03-21 17:39:44 +0900

    작업내용 : 20. useReducer - 기초 작업일시 : 2021.03.21 작업자 : 오승현

--- begin-react/src/Counter.js
+++ begin-react/src/Counter.js
@@ -1,12 +1,31 @@
-import React, {useState} from 'react';
+import React, {useReducer} from 'react';
+
+
+function reducer(state, action) {
+    switch (action.type) {
+        case 'INCREMENT':
+            return state + 1;
+        case 'DECREMENT':
+            return state - 1;
+        default:
+            throw new Error('Unhandled action');
+            
+    }
+}
 
 function Counter() {
-    const [number, setNumber] = useState(5);
+
+    const [number, dispatch] = useReducer(reducer, 0);
+    
     const onIncrease = () => {
-        setNumber(prevNumber => prevNumber + 1);
+        dispatch({
+            type: 'INCREMENT'
+        })
     };
     const onDecrease = () => {
-        setNumber(prevNumber => prevNumber + 1);
+        dispatch({
+            type: 'DECREMENT'
+        })
     };
     return (
         <div>
--- begin-react/src/index.js
+++ begin-react/src/index.js
@@ -2,12 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Counter from './Counter';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Counter />,
   document.getElementById('root')
 );
 
