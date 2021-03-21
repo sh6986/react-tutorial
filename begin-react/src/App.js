33f22ea76c73b7d04f0d750309deeaa712dcca15
seunghyun_oh <okok960411@gmail.com>
2021-03-21 19:41:58 +0900

    작업내용 : 24. UserDispatch Context 만들기 작업일시 : 2021.03.21 작업자 : 오승현

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -1,4 +1,4 @@
-import React, {useRef, useReducer, useMemo, useCallback} from 'react';
+import React, {useRef, useReducer, useMemo, useCallback, createContext} from 'react';
 import CreateUser from './CreateUser';
 import UserList from './UserList';
 import useInputs from './useInputs';
@@ -57,6 +57,8 @@ function reducer(state, action) {
   }
 }
 
+export const UserDispatch = createContext(null);
+
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
   const [form, onChange, reset] = useInputs({
@@ -80,33 +82,19 @@ function App() {
     reset();
   }, [username, email, reset]);
 
-  const onToggle = useCallback(id => {
-    dispatch({
-      type: 'TOGGLE_USER',
-      id
-    })
-  }, []);
-
-  const onRemove = useCallback(id => {
-    dispatch({
-      type: 'REMOVE_USER',
-      id
-    })
-  }, []);
-
   const count = useMemo(() => countActiveUsers(users), [users]);
 
   return (
-    <>
+    <UserDispatch.Provider value={dispatch}>
       <CreateUser 
         username={username} 
         email={email} 
         onChange={onChange}
         onCreate={onCreate}
       />
-      <UserList users={users} onToggle={onToggle} onRemove={onRemove} />
+      <UserList users={users} />
       <div>활성 사용자 수: {count}</div>
-    </>
+    </UserDispatch.Provider>
   )
 }
 
